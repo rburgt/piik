daef87c8ad5617f5f519a2b6d77efd57a190b18b
Rob van der Burgt <rburgt@gmail.com>
2012-04-17 20:58:17 +0200

    test code for dertermining location inner start of tag

--- js/piik.js
+++ js/piik.js
@@ -15,6 +15,46 @@
 		return targetObj;
 	};
 	
+	var useragent = window.navigator.userAgent.toLowerCase(),
+		browserMatch = /(webkit)[ \/]([\w.]+)/.exec( useragent ) 
+						|| useragent.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+))?/.exec( useragent ),
+		browser = {
+			name : browserMatch[1],
+			version : browserMatch[2],
+			
+			webkit : browserMatch[1] == 'webkit',
+			mozilla :  browserMatch[1] == 'mozilla'
+		};
+	
+	/**
+	 * gets all the child elements of any given element
+	 */
+	var flattenElement = function( element ){
+		var elements = [],
+			children = element.childNodes,
+			childrenCount = children.length;
+		
+		for ( var i in children ){
+			var child = children[i];
+			if ( !child.nodeType || child.nodeType !== 1 )
+				continue;
+			
+			var grandChilds = flattenElement( child );
+			grandChilds.unshift( child );
+			elements.push.apply( elements, grandChilds );
+		}
+		
+		return elements;
+	};
+	
+	/**
+	 * css tools
+	 * TODO: create mapper for css3 animations based on browser
+	 */
+	var css = {
+		
+	};
+	
 	/**
 	 * piik main class
 	 */
@@ -32,7 +72,9 @@
 		 */
 		initLayout : function(){
 			var _self = this,
-				sourceEl = this.sourceEl;
+				sourceEl = this.sourceEl,
+				sourceWidth = sourceEl.clientWidth + 'px',
+				sourceHeight = sourceEl.clientHeight + 'px';
 			
 			/**
 			 * main piik wrapper
@@ -62,13 +104,23 @@
 			piikEl.appendChild( wrapperEl );
 			
 			/**
+			 * create animated result element
+			 */
+			var animatedEl = apply(this.animatedEl = document.createElement('iframe'), {
+				className : 'result'
+			});
+			animatedEl.style.width = sourceWidth;
+			animatedEl.style.height = sourceHeight;
+			wrapperEl.appendChild( animatedEl );
+			
+			/**
 			 * create result element
 			 */
 			var resultEl = apply(this.resultEl = document.createElement('iframe'), {
 				className : 'result'
 			});
-			resultEl.style.height = sourceEl.clientHeight + 'px';
-			resultEl.style.width = sourceEl.clientWidth + 'px';
+			resultEl.style.width = sourceWidth;
+			resultEl.style.height = sourceHeight;
 			wrapperEl.appendChild( resultEl );
 			
 			/**
@@ -86,11 +138,20 @@
 		initCharacterMap : function(){
 			this.characterMap = {};
 			
+			// get the corrections for the characters style in the editor
+			var sourceStyle = getComputedStyle( this.sourceEl );
+			this.characterCorrection = {
+				left : parseInt(sourceStyle.paddingLeft.replace('px', '')),
+				top : parseInt(sourceStyle.paddingTop.replace('px', '')),
+				line : parseInt(sourceStyle.lineHeight == 'normal' ? 16 : sourceStyle.lineHeight.replace('px', ''))
+			};
+			
 			// create an element with the same style as the editor
 			// the with of the element will be used as the characters width
 			this.characterEl = document.createElement('span');
+			
 			apply(this.characterEl.style, {
-				cssText : getComputedStyle( this.sourceEl ).cssText,
+				cssText : sourceStyle.cssText,
 				padding: 0,
 				margin: 0,
 				border: 0,
@@ -184,7 +245,83 @@
 				
 			this.isEditing = false;
 			
-			this.resultEl.contentDocument.documentElement.innerHTML = this.sourceEl.value
+			var charracterCorrection = this.characterCorrection,
+				resultDocument = this.resultEl.contentDocument,
+				sourceValue = this.sourceEl.value,
+				sourceLines = sourceValue.split("\n"),
+				sourceLineCount = sourceLines.length,
+				lineIndex = 0,
+				charIndex = 0;
+			
+			resultDocument.documentElement.innerHTML = sourceValue
+			
+			// find out if there is an body tag and on wich line it starts
+			//console.log( sourceValue.match( ));
+			var bodyPos = sourceValue.search( new RegExp('<body[^>]*>([\\S\\s]*?)<\\/body>', 'gim' ));
+			
+			// only the body needs to be animated
+			if ( bodyPos !== -1 ) {
+				lineIndex = sourceValue.substr( 0, bodyPos ).split("\n").length -1;
+				charIndex = sourceLines[lineIndex].indexOf('<body');
+			}
+			
+			// thanks to http://ejohn.org/files/htmlparser.js
+			var startTag = /^<([-A-Za-z0-9_]+)((?:\s+\w+(?:\s*=\s*(?:(?:"[^"]*")|(?:'[^']*')|[^>\s]+))?)*)\s*(\/?)>/igm;
+			
+			for (; lineIndex < sourceLineCount; lineIndex++ ) {
+				var line = sourceLines[lineIndex],
+					characterCount = line.length; 
+					
+				for(; charIndex < characterCount; charIndex++ ){
+					// only test if char is <
+					if ( line.charAt( charIndex ) == '<' ){
+						// check if this tag is a valid starting tag
+						var tag = line.substring( charIndex ).match( startTag );
+						
+						if ( tag && tag.length > 0 ){
+							tag = tag[0];
+							
+							// hooray! valid tag found, now lets position it
+							charIndex += tag.length;
+							
+							var el = document.createElement('div');
+							
+							apply(el.style, {
+								padding: 0,
+								margin: 0,
+								border: 1,
+								width: '100px',
+								height: '20px',
+								backgroundColor: 'red',
+								position: 'absolute',
+								top : ((lineIndex * charracterCorrection.line ) + charracterCorrection.top) + 'px',
+								zIndex : 200,
+								left : (this.getCharactersWidth( line.substring( 0, charIndex ) ) + charracterCorrection.left) + 'px'
+							});
+							
+							
+							this.wrapperEl.appendChild( el );
+						}
+					}
+				}
+					
+				charIndex = 0;
+			}
+			
+			console.log( flattenElement( resultDocument.body ) );
+			//, flattenElement( resultDocument.body )
+			/**
+			 * css transition test
+			 */
+			var resultHead = resultDocument.getElementsByTagName('head')[0],
+				style = resultDocument.createElement('style');
+			
+			style.innerHTML = '@-webkit-keyframes globalin { 0% { background: #000; font-size: 12px; margin: 0; padding: 0; text-indent: 0; border: 0; color: #fff;} } body, * { -webkit-animation-name: globalin; -webkit-animation-duration: 1s; } ';
+			resultHead.appendChild(style);
+			
+			/**
+			 * switch result/source element zindex
+			 */
 			this.resultEl.style.zIndex = 10;
 			this.sourceEl.style.zIndex = 5;
 		}
