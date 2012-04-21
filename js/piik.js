169c5e3891b485490472b3cc5d442594c6170c29
Rob van der Burgt <rburgt@gmail.com>
2012-04-21 13:42:49 +0200

    animation/editor position detection rewritten to work with individual characters

--- js/piik.js
+++ js/piik.js
@@ -38,7 +38,7 @@
 			var computedStyle = getComputedStyle(element),
 				cssText = '';
 			
-			for (var property in computedStyle) {  
+			for (var property in computedStyle) { 
 			    if (computedStyle.hasOwnProperty(property)) {  
 			        cssText += computedStyle[property] + ':' + computedStyle.getPropertyCSSValue(computedStyle[property]).cssText + ';';
 			    } 
@@ -50,35 +50,39 @@
 		return '';
 	};
 	
-	/**
-	 * gets all the child elements of any given element
-	 */
-	var flattenElement = function( element ){
-		var elements = [],
-			children = element.childNodes,
-			childrenCount = children.length;
-		
-		for ( var i in children ){
-			var child = children[i];
-			if ( !child.nodeType || child.nodeType !== 1 )
-				continue;
-			
-			var grandChilds = flattenElement( child );
-			grandChilds.unshift( child );
-			elements.push.apply( elements, grandChilds );
-		}
-		
-		return elements;
+	var getOffsets = function( element, documentBody ){
+		documentBody = typeof documentBody == 'undefined' ? document.body : documentBody;
+	 
+	    var offsetLeft = element.offsetLeft,
+	    	offsetTop = element.offsetTop,
+	    	offsetParent = element.offsetParent;
+	 
+	    while( offsetParent != documentBody ) {
+	    	offsetLeft += offsetParent.offsetLeft;
+	        offsetTop += offsetParent.offsetTop;
+	        offsetParent = offsetParent.offsetParent;
+	    }
+	        
+	    return {
+	    	top : offsetTop,
+	    	left : offsetLeft
+	    };
 	};
-	
-	// searches for an tag beginning
+		
+	// searches for an tag beginning/endtag
 	// thanks to http://ejohn.org/files/htmlparser.js
-	var startTag = /^<([-A-Za-z0-9_]+)((?:\s+\w+(?:\s*=\s*(?:(?:"[^"]*")|(?:'[^']*')|[^>\s]+))?)*)\s*(\/?)>/igm;
+	var startTag = /^<([-A-Za-z0-9_]+)((?:\s+\w+(?:\s*=\s*(?:(?:"[^"]*")|(?:'[^']*')|[^>\s]+))?)*)\s*(\/?)>/igm,
+		endTag = /^<\/([-A-Za-z0-9_]+)[^>]*>/;
 	
 	/**
 	 * removes the standard webkit animation properties so own can be applied
 	 */
 	var noWebkitAnimations = /webkit\-animation.*;/gim;
+	
+	/**
+	 * characters not to be animated
+	 */
+	var notAnimatedChars = [' ', '\n', '\t'];
 	
 	/**
 	 * piik main class
@@ -89,6 +93,7 @@
 		this.initLayout();
 		this.initCharacterMap();
 	};
+	
 	
 	apply( piik.prototype, {
 		
@@ -279,17 +284,15 @@
 			var charracterCorrection = this.characterCorrection,
 				animationDoc = this.animatedEl.contentDocument,
 				sourceValue = this.sourceEl.value,
+				sourceLength = sourceValue.length,
 				sourceLines = sourceValue.split("\n"),
-				sourceLineCount = sourceLines.length,
+				//sourceLineCount = sourceLines.length,
 				lineIndex = 0,
 				charIndex = 0
 			
 			// push sourcecode to viewing panels
 			this.resultEl.contentDocument.documentElement.innerHTML = sourceValue
 			animationDoc.documentElement.innerHTML = sourceValue
-			
-			// get the body elements as an flat array
-			var animatedElements = flattenElement( animationDoc.body );
 			
 			// find out if there is an body tag and on wich line it starts
 			//console.log( sourceValue.match( ));
@@ -298,40 +301,75 @@
 			if ( bodyPos !== -1 ) {
 				// body tag is found, calculate an alternate starting point for source analysis
 				lineIndex = sourceValue.substr( 0, bodyPos ).split("\n").length -1;
-				charIndex = sourceLines[lineIndex].indexOf('<body');
-				
-				//add body to element list
-				animatedElements.unshift( animationDoc.body );
+				charIndex = bodyPos + sourceValue.substring( bodyPos ).match( startTag )[0].length;
 			}
 			
 			// figure out where the start tags in the source are located
-			var startTags = [];
-			for (; lineIndex < sourceLineCount; lineIndex++ ) {
-				var line = sourceLines[lineIndex],
-					characterCount = line.length; 
+			// collect the non tag characters to compare with textnodes later
+			var startTags = [],
+				textCharacters = [];
+			
+			while ( charIndex < sourceLength ) {
+				var charStartTag = sourceValue.substring( charIndex ).match( startTag );
+				if ( charStartTag && charStartTag.length > 0  )
+				{
+					charStartTag = charStartTag[0];
 					
-				for(; charIndex < characterCount; charIndex++ ){
-					// only test if char is <
-					if ( line.charAt( charIndex ) == '<' ){
-						// check if this tag is a valid starting tag
-						var tag = line.substring( charIndex ).match( startTag );
-						
-						if ( tag && tag.length > 0 ){
-							tag = tag[0];
-							
-							// hooray! valid tag found, now lets position it
-							charIndex += tag.length;
-							
-							startTags.push({
-								startTag : tag,
-								top : (((lineIndex + 1) * charracterCorrection.line ) + charracterCorrection.top),
-								left : (this.getCharactersWidth( line.substring( 0, charIndex ) ) + charracterCorrection.left)
-							});
-						}
-					}
+					
+					var lastNewLineIndex = sourceValue.substring( 0, charIndex ).lastIndexOf("\n") + 1,
+						lineText = sourceValue.substring( lastNewLineIndex, charIndex + charStartTag.length ),
+						innerCharIndex = charIndex + charStartTag.length,
+						innerLineIndex = lineIndex + charStartTag.split("\n").length -1;
+					
+					startTags.push({
+						startTag : charStartTag,
+						top : ((lineIndex * charracterCorrection.line ) + charracterCorrection.top),
+						left : (this.getCharactersWidth( lineText ) + charracterCorrection.left),
+						innerCharIndex : innerCharIndex,
+						innerLineIndex : innerLineIndex
+					});
+					
+					charIndex = innerCharIndex;
+					lineIndex = innerLineIndex;
+					
+					continue;
 				}
+				
+				var charEndTag = sourceValue.substring( charIndex ).match( endTag );
+				if ( charEndTag && charEndTag.length > 0  )
+				{
+					charEndTag = charEndTag[0];
 					
-				charIndex = 0;
+					// nothing to process anymore after body ends
+					if ( charEndTag == '</body>' )
+						break;
+					
+					charIndex += charEndTag.length;
+					lineIndex += charEndTag.split("\n").length -1;
+					continue;
+				}
+				
+				var character = sourceValue.charAt( charIndex )
+				
+				
+				if ( notAnimatedChars.indexOf( character ) == -1 ) {
+					var lastNewLineIndex = sourceValue.substring( 0, charIndex ).lastIndexOf("\n") + 1,
+						lineText = sourceValue.substring( lastNewLineIndex, charIndex )
+					
+					textCharacters.push({
+						character : character,
+						//top : lineIndex,
+						//left : charIndex - lastNewLineIndex
+						top : ((lineIndex * charracterCorrection.line ) + charracterCorrection.top),
+						left : (this.getCharactersWidth( lineText ) + charracterCorrection.left)
+					});
+				};
+				
+				if ( character == "\n" )
+					lineIndex++;
+				
+				// normal char
+				charIndex++;
 			}
 			
 			
@@ -340,17 +378,20 @@
 			 */
 			var animation = '',
 				tagLength = startTags.length,
+				tagIndex = -1,
 				usedElements = [],
-				startWidth = this.sourceEl.clientWidth + 'px';
+				startWidth = this.sourceEl.clientWidth + 'px',
+				characterIndex = 0;
+			
+			// create an tag to sniff for an character position
+			// non existing tag used so webpage stylesheet wil have no impact
+			var sniffEl = document.createElement('span');
+			
+			
+			var animateElement = function( element ){
 				
-			for ( var t = 0; t < tagLength; t++ ){
-				var tag = startTags[t],
-					element = animatedElements[t];
+				var childNodes = element.childNodes;
 				
-				// animations break on body element
-				if ( element == animationDoc.body )
-					continue;
-					
 				// get the original styling applied
 				var style = getComputedStyle( element ),
 					cssText = style.cssText;
@@ -358,109 +399,195 @@
 				// remove the original animations
 				cssText = cssText.replace(noWebkitAnimations, '');
 				
-				if (  (style.display == 'inline' || style.display == 'inline-block') ){
-					animation += [
-						'@-webkit-keyframes piik' + t + ' { 0% {',
-							'text-indent: ' + this.getCharactersWidth(tag.startTag)  + 'px;',
-							'left: 0px;',
-							'font-size: 12px;',
-							'font-weight: normal;',
-							'margin: auto;',
-							'padding: auto;',
-							'border: auto;',
-							'width: ' + startWidth + ';',
-							'background-color: transparent;',
-						'} } ',
-						'.piikoriginal' + t + ' {',
-							cssText,
-						'}',
-						'.piik' + t + ' {',
-							'-webkit-animation-name: piik' + t + ';',
-							'-webkit-animation-duration: 1.2s;', 
-							'-webkit-animation-timing-function: ease-in;', 
-						'}'
-					].join("\n");
+				if ( element != animationDoc.body ){
+					tagIndex++;
+					var tag = startTags[tagIndex],
+						t = tagIndex;
 					
-				}
-				else
-				{
-					animation += [
-						'.piikoriginal' + t + ', .piikplaceholder' + t + ' {',
-							cssText,
-						'} ',
-						
-						'.piikplaceholder' + t + ' {',
-							'-webkit-animation-name: piikplaceholder' + t + ';',
-							'-webkit-animation-duration: .6s;', 
-							'-webkit-animation-timing-function: ease-in;', 
-							'visibility: hidden;',
-							'width: ' + element.offsetWidth  + 'px;',
-							'height: ' + element.offsetHeight + 'px;',
-							'display: inline-block;',
-						'} ',
-						
-						'@-webkit-keyframes piikplaceholder' + t + ' { 0% {',
-							'margin: auto;',
-							'padding: auto;',
-							'border: auto;',
-							'width: auto;',
-						'} } ',
-						
-						'@-webkit-keyframes piik' + t + ' { 0% {',
-							//'position: absolute;',
-							'top: ' + tag.top  + 'px;',
-							'text-indent: ' + tag.left  + 'px;',
-							'left: 0px;',
-							'font-size: 12px;',
-							'margin: auto;',
-							'padding: auto;',
-							'border: auto;',
-							'width: ' + startWidth + ';',
-							'background-color: transparent;',
-							//'text-align: left;',
-						'} } ',
-						
-						'.piik' + t + ' {',
-							'-webkit-animation-name: piik' + t + ';',
-							'-webkit-animation-duration: 1.2s;', 
-							'-webkit-animation-timing-function: ease-in;', 
-							'position: absolute;',
-							'top: ' + element.offsetTop  + 'px;',
-							'left: ' + element.offsetLeft  + 'px;',
-							'width: ' + element.offsetWidth  + 'px;',
-							'height: ' + element.offsetHeight + 'px;',
-							'margin: 0px;',
-							'padding: 0px;',
-							'border: 0px;',
-						'}'
-					].join("\n");
 					
-					var placeholderEl = document.createElement('div');
-					placeholderEl.className = 'piikplaceholder' + t;
-					
-					element.placeholderEl = placeholderEl;
-					
+					if ( style.display == 'block' || style.display == 'inline-block' ){
+						var elementOffsets = getOffsets( element, animationDoc.body );
+						
+						animation += [
+							'.piikoriginal' + t + ' {',
+								cssText,
+							'} ',
+							'@-webkit-keyframes piik' + t + ' { 0% {',
+								//'position: absolute;',
+								'top: ' + tag.top  + 'px;',
+								'text-indent: ' + tag.left  + 'px;',
+								'left: 0px;',
+								'font-size: 12px;',
+								'margin: auto;',
+								'padding: auto;',
+								'border: auto;',
+								'width: ' + startWidth + ';',
+								'background-color: transparent;',
+								//'text-align: left;',
+							'} } ',
+							
+							'.piik' + t + ' {',
+								'-webkit-animation-name: piik' + t + ';',
+								'-webkit-animation-duration: 800ms;', 
+								'-webkit-animation-timing-function: ease-in;', 
+								'position: absolute;',
+								'top: ' + elementOffsets.top  + 'px;',
+								'left: ' + elementOffsets.left  + 'px;',
+								'width: ' + element.offsetWidth  + 'px;',
+								'height: ' + element.offsetHeight + 'px;',
+								'margin: 0px;',
+								'padding: 0px;',
+								'border: 0px;',
+							'}'
+						].join("\n");
+						
+						// generated class can not be applied directly because it will interfere
+						// with childNode styling
+						element.piikClass = 'piikoriginal' + t + ' piik' + t;
+					}
+						
 					usedElements.push( element );
 				}
 				
-				// generated class can not be applied directly because it will interfere
-				// with childNode styling
-				element.piikClass = 'piikoriginal' + t + ' piik' + t;
-			}
+				// childNodes.length need to be calculated every time
+				// because character position detection will add nodes
+				for ( var i = 0; i < childNodes.length; i++ ){
+					var node = childNodes[i];
+					
+					if ( !node.nodeType )
+						continue;
+					
+					switch( node.nodeType ){
+						case 1:
+							animateElement.call(this, node );
+							break;
+							
+						case 3:
+							// clone style of element for character position sniffing
+							sniffEl.style.cssText = cssText;
+							// prevent document style from overriding element positioning
+							apply( sniffEl.style, {
+								margin : '0',
+								padding: '0',
+								border: '0',
+								display: 'inline',
+								'float': 'none',
+								position: 'static',
+								border: '0',
+								height: 'auto',
+								width: 'auto'
+							} );
+							
+							// detect individual positions of characters
+							var nodedata = node.data,
+								nodeLenght = nodedata.length,
+								range = animationDoc.createRange(),
+								charUnusedIndex = 0;
+								
+							for ( var ni = 0; ni < nodeLenght; ni++ ){
+								var charString = nodedata.charAt(ni);
+								
+								if ( notAnimatedChars.indexOf( charString ) != -1 ){
+									charUnusedIndex++;
+									continue;
+								}
+								
+								// get the calculated character information 
+								var characterInfo = textCharacters[characterIndex],
+									characterElement = sniffEl.cloneNode(false);
+								
+								characterIndex++;
+								characterElement.innerHTML = charString;
+								characterElement.allowData = true;
+								
+								range.setStart( node, charUnusedIndex +1 );
+								range.setEnd( node, charUnusedIndex + 1 );
+								range.insertNode( characterElement );
+								
+								console.log( charString, characterInfo.top, characterInfo.left, characterElement.offsetTop, characterElement.offsetLeft );
+								
+								
+								var characterOffsets = getOffsets( characterElement, animationDoc.body );
+								
+								animation += [
+									'.piikcharoriginal' + characterIndex + ' {',
+										cssText,
+									'} ',
+									'@-webkit-keyframes piikchar' + characterIndex + ' { 0% {',
+										//'position: absolute;',
+										'top: ' + characterInfo.top  + 'px;',
+										'left: ' + characterInfo.left  + 'px;',
+										'font-size: 12px;',
+										'margin: auto;',
+										'padding: auto;',
+										'border: auto;',
+										'background-color: transparent;',
+										//'text-align: left;',
+									'} } ',
+									
+									'.piikchar' + characterIndex + ' {',
+										'-webkit-animation-name: piikchar' + characterIndex + ';',
+										'-webkit-animation-duration: 800ms;', 
+										'-webkit-animation-timing-function: ease-in;', 
+										'position: absolute;',
+										'top: ' + characterOffsets.top  + 'px;',
+										'left: ' + (characterOffsets.left - characterElement.offsetWidth )  + 'px;',
+										'display: block;',
+										'margin: 0px;',
+										'padding: 0px;',
+										'border: 0px;',
+										'text-indent: 0px;',
+										'width: auto;',
+										'height: auto;',
+										'background: none;',
+									'}'
+								].join("\n");
+								
+								// generated class can not be applied directly because it will interfere
+								// with childNode styling
+								characterElement.piikClass = 'piikcharoriginal' + characterIndex + ' piikchar' + characterIndex;
+								usedElements.push( characterElement );
+								
+								element.removeChild( characterElement );
+								
+								charUnusedIndex = 0;
+								i++;
+								node = childNodes[i];
+							}
+							break;
+					}
+				}
+			};
+			
+			animateElement.call(this, animationDoc.body);
+			
 			
 			// append all used elements directly to the body to 
 			// allow correct absolute positioning
 			usedElements.forEach(function( element ){
-				console.log(element.innerHTML);
 				element.style.cssText = '';
 				element.id = '';
-				//element.parentNode.removeChild(element);
-				if ( element.placeholderEl ){
-					element.parentNode.insertBefore( element.placeholderEl, element );
+				
+				var childNodes = element.childNodes,
+					childLength = childNodes.length;
+				
+				// clear the original textNode data of the element
+				if ( !element.allowData ){
+					for ( var x = 0; x < childLength; x++ ){
+						var node = childNodes[x];
+						
+						// check if node is an text node
+						if ( node.nodeType != 3 )
+							continue;
+							
+						node.data = '';
+					}
 				}
 				
 				animationDoc.body.appendChild( element );
-				element.className = element.piikClass;
+				
+				if ( element.piikClass )
+					element.className = element.piikClass;
 			});
 			
 			// background fade animation
